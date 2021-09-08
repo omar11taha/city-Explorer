3a982c085ea9639df5c5f8b33f537dc0c283c0cf
omar11taha <omartawaha95@gmail.com>
2021-09-08 17:43:34 +0300

    lab07

--- src/App.js
+++ src/App.js
@@ -1,87 +1,114 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: {},
-      error:false,
+      locationData: {},
+      error: false,
+      locationImg: "",
+      locationInfo:[],
     };
   }
-
   submitForm = async (e) => {
     e.preventDefault();
-    try{
-    const location = e.target.name.value;
-    console.log('user Input Location: ', location);
-    const response = await axios.get(
-      `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_key}&q=${location}&format=json`
+    try {
+      const location = e.target.locationName.value;
+      console.log("user Input Location: ", location);
+      const response = await axios.get(
+        `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${location}&format=json`
       );
-      console.log(e.target.name.value);
+      this.setState({
+        locationData: response.data[0],
+      });
+      const response2 = await axios.get(
+        `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.state.locationData.lat},${this.state.locationData.lon}&format=jpg `
+      );
+      
+      console.log("our axios response", response.data[0]);
+      this.setState({
+        locationImg: response2.config.url,
+      });
 
-    console.log("our axios response", response.data);
+      console.log(location);
+      const responses = await axios.get(
+        
+        `${process.env.REACT_APP_SERVER_URL}/weather?city_name=${location}`,
+      );
 
-    this.setState({
-      location: response.data[0],
-    });
-  }catch(error){
-    this.setState({
-     error:true,
-    });
+      this.setState({
+        locationInfo: responses.data,
+      });
 
-
-  }};
-
+      
+    } catch (error) {
+      console.log("catch error" + error);
+      this.setState({
+        error: true,
+      });
+    }
+  };
   render() {
-let locations
-    if (this.state.location.lat) {
-      locations =  `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_key}&center=${this.state.location.lat},${this.state.location.lon}&format=jpg `;
+   
 
-    }
-    else{
-      locations = "";
-    }
+
     return (
       <div>
-        <Form onSubmit={this.submitForm}>
-  <Form.Group  className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Location Name:</Form.Label>
-    <Form.Control name="name" type="text" placeholder="Enter location" />
-    
-  </Form.Group>
-
-  
-  <Button variant="primary" type="submit">
-  search
-  </Button>
-</Form>
-        {/* <form onSubmit={this.submitForm}>
-          <label>Location Name:</label>
-          <input name="Name" type="text" placeholder="Enter location" />
-          <input type="submit" value="search " />
-        </form> */}
-      
-          {!this.state.error&&
-            <div>
-          <h1>Location information</h1>
-          <p>{this.state.location.display_name}</p>
-          <p>{this.state.location.lon}</p>
-            <p>{this.state.location.lat}</p>
-        <img src={locations} alt={""} />
-            </div>}
-            {this.state.error &&<p>error getting the data  </p>}
-            
-
-       
-        
-        
-
+        <form onSubmit={this.submitForm}>
+          <label
+            style={{
+              color: "white",
+            }}
+          >
+            Location Name:{" "}
+          </label>
+          <input name="locationName" type="text" placeholder="Enter Location" />
+          <input type="submit" value="Search Location" />
+        </form>
+        <div>
+          <Card
+            style={{
+              width: "30rem",
+              border: "solid",
+              background: "white",
+              // color: "white",
+            }}
+          >
+            <Card.Body
+              style={{
+                color: "black",
+              }}
+            >
+              <Card.Title>
+                {" "}
+                Display Name :{this.state.locationData.display_name}
+              </Card.Title>
+              <Card.Text>latitude : {this.state.locationData.lat}</Card.Text>
+              <Card.Text>longitude : {this.state.locationData.lon}</Card.Text>
+              {this.state.locationInfo.map(element=>{
+              return   <Card.Text>
+                
+                 locationInfo : {element.description}{" "}
+                {element.date}
+              </Card.Text> })   }
+            </Card.Body>
+          </Card>
+        </div>
+        <div>
+          {this.state.error && (
+            <p > try again </p>
+          )}
+          {this.state.locationData.lon && (
+            <p >The Selected  </p>
+          )}
+        </div>
+        <div>
+          <img src={this.state.locationImg} alt={""} />
+        </div>
       </div>
     );
   }
 }
-
 export default App;
